9d44477ac1f6049ab44cccc426c01a2f46aef64c
Rutger van Rees <r.vanrees@icloud.com>
2023-11-14 17:04:55 +0100

    Update Quote.tsx

--- src/app/components/Quote.tsx
+++ src/app/components/Quote.tsx
@@ -4,14 +4,20 @@ const Quote = () => {
       <figure>
         <blockquote className="text-center text-xl font-semibold leading-8 sm:text-2xl sm:leading-10 text-white">
           <p>
-            “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-            expedita{" "}
+            “From phones to cars to medicine, technology touches every part of
+            our lives. If you can create technology, you can{" "}
             <span className="bg-secondary px-2 text-charcoal text-3xl">
-              voluptas
+              change
             </span>{" "}
-            culpa sapiente alias molestiae. Numquam corrupti in laborum sed
-            rerum et corporis.”
+            the world.”
           </p>
+          <a
+            href="https://en.wikipedia.org/wiki/Susan_Wojcicki"
+            target="_blank"
+            className="mt-1 italic text-xs text-chalk font-normal hover:text-secondary transition"
+          >
+            Susan Wojcicki
+          </a>
         </blockquote>
       </figure>
     </div>
