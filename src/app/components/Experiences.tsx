490ca61991520b882ea1113a3b255c41dac91ead
Rutger van Rees <r.vanrees@icloud.com>
2023-11-15 14:41:46 +0100

    Fixed errors

--- src/app/components/Experiences.tsx
+++ src/app/components/Experiences.tsx
@@ -3,8 +3,8 @@
 import { useEffect, useState } from "react";
 
 // For loading state
-const placeholder = [...Array(3)];
-const lag = 1200;
+const placeholder: number[] = [1, 2, 3];
+const lag: number = 1200;
 
 const Experiences = () => {
   const [experiences, setExpierences] = useState<Experience[]>([]);
@@ -46,18 +46,22 @@ const Experiences = () => {
           </p>
         )}
         <h2 className="text-3xl font-bold tracking-tight text-white">
-          I do have experience with
+          {isLoading ? (
+            <div className="my-2 text-lg font-semibold leading-6 rounded bg-slate-700 h-8 w-64 animate-pulse"></div>
+          ) : (
+            "Lorem ipsun dolor sit amet"
+          )}
         </h2>
         <div className="mt-2 mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-3 pt-4">
           {isLoading
-            ? placeholder.map((_p: any, i: number) => (
+            ? placeholder.map((p: number) => (
                 <article
-                  key={i}
+                  key={p}
                   className="flex max-w-xl flex-col items-start animate-pulse"
                 >
                   <div className="group relative">
                     <div className="flex flex-row">
-                      <h3 className="mt-2 text-lg font-semibold leading-6 rounded bg-slate-700 h-6 w-32"></h3>
+                      <p className="mt-2 text-lg font-semibold leading-6 rounded bg-slate-700 h-6 w-32"></p>
                     </div>
                     <p className="mt-2 text-sm font-normal rounded leading-6 bg-slate-700 h-4 w-64"></p>
                     <p className="mt-2 text-sm font-normal rounded leading-6 bg-slate-700 h-4 w-48"></p>
@@ -72,30 +76,31 @@ const Experiences = () => {
                   key={e.id}
                   className="flex max-w-xl flex-col items-start"
                 >
-                  <div className="group relative">
-                    <div className="flex flex-row">
-                      <h3 className="mt-2 text-lg font-semibold leading-6 text-white">
-                        {e.title}
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          fill="currentColor"
-                          className="w-6 h-6 -mt-0.5 ml-1 text-secondary inline"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                      </h3>
-                    </div>
-                    {e.content.map((p: string) => (
-                      <p className="mt-2 text-sm font-normal leading-6 text-chalk">
-                        {p}
-                      </p>
-                    ))}
+                  <div className="flex flex-row">
+                    <h3 className="mt-2 text-lg font-semibold leading-6 text-white">
+                      {e.title}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-6 h-6 -mt-0.5 ml-1 text-secondary inline"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </h3>
                   </div>
+                  {e.content.map((p: string, i: number) => (
+                    <p
+                      key={i}
+                      className="mt-2 text-sm font-normal leading-6 text-chalk"
+                    >
+                      {p}
+                    </p>
+                  ))}
                 </article>
               ))}
         </div>
