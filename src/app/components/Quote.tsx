443b689d4a69773533e800b74de3739bc9d6f49b
Rutger van Rees <r.vanrees@icloud.com>
2023-11-16 14:47:22 +0100

    Update Quote.tsx

--- src/app/components/Quote.tsx
+++ src/app/components/Quote.tsx
@@ -3,14 +3,12 @@ const Quote = () => {
     <div className="mx-auto max-w-2xl lg:max-w-4xl px-6 py-24 lg:px-8">
       <figure>
         <blockquote className="text-center text-xl font-semibold leading-8 sm:text-2xl sm:leading-10 text-white font-serif">
-          <p>
-            “From phones to cars to medicine, technology touches every part of
-            our lives. If you can create technology, you can{" "}
-            <span className="bg-secondary px-2 pb-1 text-charcoal text-2xl sm:text-3xl">
-              change
-            </span>{" "}
-            the world.”
-          </p>
+          “From phones to cars to medicine, technology touches every part of our
+          lives. If you can create technology, you can{" "}
+          <span className="bg-secondary px-2 pb-1 text-charcoal text-2xl sm:text-3xl hover:bg-white cursor-crosshair transition">
+            change
+          </span>{" "}
+          the world.”
           <a
             href="https://en.wikipedia.org/wiki/Susan_Wojcicki"
             target="_blank"
